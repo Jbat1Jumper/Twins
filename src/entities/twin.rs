29e82879709574899c7ef90616b035e50822a486
Nikita Zdanovitch <nzdanovitch@gmail.com>
2018-03-28 20:02:42 -0300

    Twin message handler to move left or right.

--- src/entities/twin.rs
+++ src/entities/twin.rs
@@ -5,7 +5,7 @@ use ggez::graphics::Color;
 
 use palette::Palette;
 use entities::{Entity, EntityData};
-use messages::{MessageSender, Message};
+use messages::{MessageSender, Message, Direction};
 
 use math::VectorUtils;
 
@@ -16,6 +16,8 @@ const PRECISION : f32 = 0.5;
 pub struct Twin {
     entity_data: EntityData,
     cycle: f32,
+    going_to: Option<Point2>,
+    speed: f32,
 }
 
 impl Entity for Twin {
@@ -24,10 +26,19 @@ impl Entity for Twin {
     fn entity_data(&self) -> &EntityData { &self.entity_data }
     fn update(&mut self, _ctx: &mut Context) {
         self.cycle += 0.1;
+        if let Some(vector) = self.going_to {
+            let delta = vector.unit().mul(self.speed);
+            let pos = self.get_pos();
+            self.set_pos(pos.add(delta));
+
+            self.going_to = match vector.norm() > delta.norm() {
+                true => Some(vector.sub(delta)),
+                false => None,
+            }
+        }
     }
     fn render(&mut self, ctx: &mut Context) {
-        let cycle = self.cycle;
-        graphics::set_color(ctx, Color::from(Palette::Player));
+        graphics::set_color(ctx, Color::from(Palette::Player)).unwrap();
         graphics::circle(
             ctx,
             DrawMode::Fill,
@@ -36,8 +47,17 @@ impl Entity for Twin {
             PRECISION
         ).unwrap();
     }
-    fn receive_message(&mut self, sender: MessageSender, message: Message) {
-
+    fn receive_message(&mut self, _sender: MessageSender, message: Message) {
+        match message {
+            Message::Move(direction, distance) => {
+                self.going_to = match direction {
+                    Direction::Right => Some(Point2::new(distance, 0.0)),
+                    Direction::Left => Some(Point2::new(-distance, 0.0)),
+                    _ => None
+                };
+            }
+            _ => ()
+        }
     }
 }
 
@@ -48,7 +68,9 @@ impl Twin {
                 pos,
                 ..EntityData::new()
             },
-            cycle: 0.0
+            cycle: 0.0,
+            going_to: None,
+            speed: 2.0,
         }
     }
 }
