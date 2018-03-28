c9047fadac07bceec937136a52ac7280c42d0f5c
Nikita Zdanovitch <nzdanovitch@gmail.com>
2018-03-28 20:04:11 -0300

    More vector operations for Point2.

--- src/math.rs
+++ src/math.rs
@@ -1,16 +1,32 @@
 use ggez::graphics::Point2;
+use std::marker::Sized;
 
-pub trait VectorUtils {
-    fn rotate(self, angle: f32) -> Self;
-    fn add(self, other: Self) -> Self;
+pub trait VectorUtils where Self: Sized {
+    fn rotate(&self, angle: f32) -> Self;
+    fn add(&self, other: Self) -> Self;
+    fn mul(&self, scalar: f32) -> Self;
+    fn sub(&self, other: Self) -> Self {
+        self.add(other.mul(-1.0))
+    }
+    fn norm(&self) -> f32;
+    fn unit(&self) -> Self {
+        let norm = self.norm();
+        self.mul(1.0/norm)
+    }
 }
 
 impl VectorUtils for Point2 {
-    fn rotate(self, angle: f32) -> Point2 {
+    fn rotate(&self, angle: f32) -> Point2 {
         use na::geometry::Rotation2;
         Rotation2::new(angle) * self
     }
-    fn add(self, other: Point2) -> Point2 {
+    fn add(&self, other: Point2) -> Point2 {
         Point2::new(self.x + other.x, self.y + other.y)
+    }
+    fn mul(&self, scalar: f32) -> Point2 {
+        Point2::new(self.x * scalar, self.y * scalar)
+    }
+    fn norm(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
     }
 }
