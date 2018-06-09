ccb45916b883353dbef97ece498c89270ee672e1
Nikita Zdanovitch <nzdanovitch@gmail.com>
2018-06-09 19:42:28 -0300

    Rust fmt in bezier

--- src/bezier.rs
+++ src/bezier.rs
@@ -1,7 +1,7 @@
-use ggez::Context;
-use ggez::graphics::Point2;
-use math::VectorUtils;
 use entities::Renderable;
+use ggez::graphics::Point2;
+use ggez::Context;
+use math::VectorUtils;
 
 pub struct Bezier {
     points: Vec<Point2>,
@@ -16,7 +16,7 @@ impl Bezier {
         Bezier {
             points: v,
             c1_points: Vec::new(),
-            c2_points: Vec::new()
+            c2_points: Vec::new(),
         }
     }
     pub fn to(mut self, c1: Point2, c2: Point2, d: Point2) -> Self {
@@ -29,11 +29,9 @@ impl Bezier {
     pub fn get(&self, tt: f32) -> Point2 {
         if self.points.len() == 0 {
             *self.points.get(0).unwrap()
-        }
-        else if tt >= 1.0 {
-            *self.points.get(self.points.len()-1).unwrap()
-        }
-        else {
+        } else if tt >= 1.0 {
+            *self.points.get(self.points.len() - 1).unwrap()
+        } else {
             let n = self.c1_points.len() as f32;
             let i = (tt * n).floor() as usize;
             let t = (tt * n) - i as f32;
@@ -41,11 +39,11 @@ impl Bezier {
             let o = self.points.get(i).unwrap();
             let c1 = self.c1_points.get(i).unwrap();
             let c2 = self.c2_points.get(i).unwrap();
-            let d = self.points.get(i+1).unwrap();
+            let d = self.points.get(i + 1).unwrap();
 
-            let a1 = o.mul((1.0-t).powi(3));
-            let a2 = c1.mul(3.0*(1.0-t).powi(2)*t);
-            let a3 = c2.mul(3.0*(1.0-t)*t.powi(2));
+            let a1 = o.mul((1.0 - t).powi(3));
+            let a2 = c1.mul(3.0 * (1.0 - t).powi(2) * t);
+            let a3 = c2.mul(3.0 * (1.0 - t) * t.powi(2));
             let a4 = d.mul(t.powi(3));
 
             a1.add(a2).add(a3).add(a4)
@@ -64,7 +62,9 @@ impl Renderable for Bezier {
 
         let lines = self.points.len() * precision;
 
-        let v = (0..lines+1).map(|x| self.get(x as f32 / lines as f32)).collect::<Vec<Point2>>();
+        let v = (0..lines + 1)
+            .map(|x| self.get(x as f32 / lines as f32))
+            .collect::<Vec<Point2>>();
 
         graphics::line(ctx, &v, 1.0).unwrap();
     }
@@ -72,8 +72,11 @@ impl Renderable for Bezier {
 
 #[test]
 fn bezier_with_two_points() {
-    let b = Bezier::from(Point2::new(0.0, 0.0))
-        .to(Point2::new(1.0, 0.0), Point2::new(1.0, 1.0), Point2::new(0.0, 1.0));
+    let b = Bezier::from(Point2::new(0.0, 0.0)).to(
+        Point2::new(1.0, 0.0),
+        Point2::new(1.0, 1.0),
+        Point2::new(0.0, 1.0),
+    );
     assert_eq!(b.get(0.0), Point2::new(0.0, 0.0));
     assert_eq!(b.get(0.5), Point2::new(0.75, 0.5));
     assert_eq!(b.get(1.0), Point2::new(0.0, 1.0));
@@ -82,8 +85,16 @@ fn bezier_with_two_points() {
 #[test]
 fn bezier_with_three_points() {
     let b = Bezier::from(Point2::new(0.0, 0.0))
-        .to(Point2::new(1.0, 0.0), Point2::new(1.0, 1.0), Point2::new(0.0, 1.0))
-        .to(Point2::new(-1.0, 1.0), Point2::new(-1.0, 2.0), Point2::new(0.0, 2.0));
+        .to(
+            Point2::new(1.0, 0.0),
+            Point2::new(1.0, 1.0),
+            Point2::new(0.0, 1.0),
+        )
+        .to(
+            Point2::new(-1.0, 1.0),
+            Point2::new(-1.0, 2.0),
+            Point2::new(0.0, 2.0),
+        );
     assert_eq!(b.get(0.0), Point2::new(0.0, 0.0));
     assert_eq!(b.get(0.25), Point2::new(0.75, 0.5));
     assert_eq!(b.get(0.5), Point2::new(0.0, 1.0));
