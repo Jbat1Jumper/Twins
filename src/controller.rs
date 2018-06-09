39eb223f6c1583c3de51a40cca2c7f69df7b34c4
Nikita Zdanovitch <nzdanovitch@gmail.com>
2018-06-09 19:44:55 -0300

    fmt: reorder use statements, add trailing commas, separate long function calls.

--- src/controller.rs
+++ src/controller.rs
@@ -1,8 +1,8 @@
+use entities::{EntityTag, EntityTagPlayer};
 use ggez::event::{Keycode, Mod};
 use ggez::graphics::Point2;
 use math::VectorUtils;
-use entities::{EntityTag, EntityTagPlayer};
-use messages::{SendMessageTo, Message, Direction};
+use messages::{Direction, Message, SendMessageTo};
 use Game;
 
 pub struct Controller {
@@ -14,14 +14,17 @@ impl Controller {
     pub fn new() -> Controller {
         Controller {
             p1_axis: Point2::zero(),
-            p1_motion_axis: Point2::zero()
+            p1_motion_axis: Point2::zero(),
         }
     }
     pub fn update(&mut self, game: &mut Game) {
         self.update_motion();
         if self.p1_motion_axis.norm() >= 0.1 {
             println!("p1_axis {:?}", self.p1_motion_axis);
-            game.send_message(EntityTag::Player(EntityTagPlayer::One), Message::Move(Direction::Point(self.p1_motion_axis), 5.0));
+            game.send_message(
+                EntityTag::Player(EntityTagPlayer::One),
+                Message::Move(Direction::Point(self.p1_motion_axis), 5.0),
+            );
         }
     }
     pub fn key_down_event(&mut self, _game: &mut Game, keycode: Keycode, _keymod: Mod) {
@@ -40,7 +43,7 @@ impl Controller {
             Keycode::S => Point2::down(),
             Keycode::D => Point2::right(),
             Keycode::W => Point2::up(),
-            _ => Point2::zero()
+            _ => Point2::zero(),
         }
     }
     fn update_motion(&mut self) {
