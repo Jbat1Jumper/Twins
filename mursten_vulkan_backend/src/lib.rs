84fe7038174f68a5410c4b13914cae0701d643af
Nikita Zdanovitch <nzdanovitch@gmail.com>
2018-08-09 22:58:11 -0300

    Cull back faces.

--- mursten_vulkan_backend/src/lib.rs
+++ mursten_vulkan_backend/src/lib.rs
@@ -315,6 +315,7 @@ where
                 .vertex_shader(vs.main_entry_point(), ())
                 .triangle_list()
                 .viewports_dynamic_scissors_irrelevant(1)
+                .cull_mode_back()
                 .fragment_shader(fs.main_entry_point(), ())
                 .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
                 .build(device.clone())
