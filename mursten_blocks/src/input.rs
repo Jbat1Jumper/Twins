526f736c9bfed979b0039ad9cab6a073358b08c1
Nikita Zdanovitch <nzdanovitch@gmail.com>
2018-08-26 11:46:44 -0300

    OnKeyboard/Mouse input traits for mursten_blocks.

---
+++ mursten_blocks/src/input.rs
@@ -0,0 +1,52 @@
+use mursten::Data;
+use nalgebra::{Point2, Vector2};
+
+
+pub trait OnKeyboard {
+    fn handle(&mut self, ev: KeyboardEvent) {}
+}
+
+pub enum KeyboardEvent {
+    Pressed(Key, KeyModifiers),
+    Released(Key, KeyModifiers),
+}
+
+pub struct KeyModifiers {
+}
+
+pub enum Key {
+    A,
+    S,
+    D,
+    Q,
+    W,
+    E,
+}
+
+pub trait OnMouse {
+    fn handle(&mut self, ev: MouseEvent) {}
+}
+
+pub enum MouseEvent {
+    Pressed(MouseButton, Point2<f32>),
+    Released(MouseButton, Point2<f32>),
+    Movement(Vector2<f32>, Point2<f32>),
+    Wheel(Vector2<f32>),
+}
+
+pub enum MouseButton {
+    Left,
+    Right,
+    Middle,
+}
+
+
+pub trait NoKeyboard {}
+pub trait NoMouse {}
+
+impl<D> OnKeyboard for D
+where D: Data + NoKeyboard {}
+
+impl<D> OnMouse for D
+where D: Data + NoMouse {}
+
--- mursten_blocks/src/lib.rs
+++ mursten_blocks/src/lib.rs
@@ -1,7 +1,9 @@
 extern crate midir;
 extern crate mursten;
 extern crate rustyline;
+extern crate nalgebra;
 
+pub mod input;
 pub mod midi;
 pub mod properties;
 pub mod property_editor;
