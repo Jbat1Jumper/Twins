cb2d023736d6f4ae990414e14c79adad9c062f3a
Nikita Zdanovitch <nzdanovitch@gmail.com>
2018-08-26 12:01:20 -0300

    Explode mursten vulkan backend code.

---
+++ mursten_vulkan_backend/src/geometry.rs
@@ -0,0 +1,87 @@
+use nalgebra::geometry::{Point2, Point3};
+
+#[derive(Debug, Clone, Copy)]
+pub struct Vertex {
+    pub position: Point3<f32>,
+    pub color: [f32; 4],
+    pub texture: [f32; 2],
+}
+
+impl Default for Vertex {
+    fn default() -> Self {
+        Vertex {
+            position: Point3::origin(),
+            color: [1.0, 1.0, 1.0, 1.0],
+            texture: [0.0, 0.0],
+        }
+    }
+}
+
+impl Vertex {
+    pub fn at(position: Point3<f32>) -> Self {
+        Vertex { position, ..Self::default() }
+    }
+
+    pub fn color(self: Vertex, r: f32, g: f32, b: f32, a: f32) -> Vertex {
+        Vertex {
+            color: [r, g, b, a],
+            ..self
+        }
+    }
+}
+
+
+impl From<Point2<f32>> for Vertex {
+    fn from(position: Point2<f32>) -> Self {
+        Vertex {
+            position: Point3::new(position.x, position.y, 0.0),
+            ..Self::default()
+        }
+    }
+}
+
+impl From<Point3<f32>> for Vertex {
+    fn from(position: Point3<f32>) -> Self {
+        Vertex::at(position)
+    }
+}
+
+pub struct Triangle {
+    pub v1: Vertex,
+    pub v2: Vertex,
+    pub v3: Vertex,
+}
+
+impl Triangle {
+    pub fn new(v1: Vertex, v2: Vertex, v3: Vertex) -> Self {
+        Triangle { v1, v2, v3 }
+    }
+}
+
+use std::vec;
+
+impl IntoIterator for Triangle {
+    type Item = Vertex;
+    type IntoIter = vec::IntoIter<Vertex>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        vec![self.v1, self.v2, self.v3].into_iter()
+    }
+}
+
+impl Default for Triangle {
+    fn default() -> Self {
+        Triangle {
+            v1: Vertex::default(),
+            v2: Vertex::default(),
+            v3: Vertex::default(),
+        }
+    }
+}
+
+use nalgebra::Transform3;
+
+pub struct Mesh {
+    pub triangles: Vec<Triangle>,
+    pub transform: Transform3<f32>,
+}
---
+++ mursten_vulkan_backend/src/shaders.rs
@@ -0,0 +1,56 @@
+
+pub mod vs {
+    #[derive(VulkanoShader)]
+    #[ty = "vertex"]
+    #[src = "
+        #version 450
+        
+        const float PI = 3.1415926535897932384626433832795;
+        const float PI_2 = 1.57079632679489661923;
+        const float PI_4 = 0.785398163397448309616;
+
+        layout(location = 0) in vec4 position;
+        layout(location = 4) in vec4 color;
+        layout(location = 8) in vec2 texture;
+        layout(location = 0) out vec4 outColor;
+
+        layout(push_constant) uniform pushConstants {
+            mat4 world;
+            mat4 view;
+            mat4 projection;
+            float scale;
+        } c;
+
+        void main() {
+            mat4 scale = mat4(
+                c.scale, 0, 0, 0,
+                0, c.scale, 0, 0,
+                0, 0, c.scale, 0,
+                0, 0, 0, 1
+            );
+            gl_Position = c.projection * c.view * c.world * scale * position;
+            outColor = color;
+        }
+    "]
+    struct Dummy;
+}
+
+pub mod fs {
+    #[derive(VulkanoShader)]
+    #[ty = "fragment"]
+    #[src = "
+        #version 450
+
+        layout(location = 0) in vec4 inColor;
+        layout(location = 0) out vec4 outColor;
+
+        float rand(vec2 co) {
+            return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+        }
+
+        void main() {
+            outColor = inColor;
+        }
+    "]
+    struct Dummy;
+}
