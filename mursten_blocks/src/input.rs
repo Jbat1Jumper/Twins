c457f1f65a6cfed4945830665c3035dd71541267
Nikita Zdanovitch <nzdanovitch@gmail.com>
2018-08-29 00:35:11 -0300

    Improve input block and add an updater between them to separate the domains.

--- mursten_blocks/src/input.rs
+++ mursten_blocks/src/input.rs
@@ -1,8 +1,8 @@
-use mursten::Data;
+use mursten;
 use nalgebra::{Point2, Vector2};
 
 pub trait OnKeyboard {
-    fn handle(&mut self, ev: KeyboardEvent) {}
+    fn handle(&mut self, _: KeyboardEvent) {}
 }
 
 pub enum KeyboardEvent {
@@ -22,7 +22,7 @@ pub enum Key {
 }
 
 pub trait OnMouse {
-    fn handle(&mut self, ev: MouseEvent) {}
+    fn handle(&mut self, _: MouseEvent) {}
 }
 
 pub enum MouseEvent {
@@ -38,17 +38,62 @@ pub enum MouseButton {
     Middle,
 }
 
-pub trait NoKeyboard {}
-pub trait NoMouse {}
+pub struct KeyboardUpdater {}
 
-impl<D> OnKeyboard for D
-where
-    D: Data + NoKeyboard,
-{
+impl KeyboardUpdater {
+    pub fn new() -> Self {
+        KeyboardUpdater {}
+    }
 }
 
-impl<D> OnMouse for D
+impl<B, D> mursten::Updater<B, D> for KeyboardUpdater
 where
-    D: Data + NoMouse,
+    D: mursten::Data + OnKeyboard,
+    B: mursten::Backend<D> + backend::KeyboardEventSource,
 {
+    fn update(&mut self, backend: &mut B, data: &mut D) {
+        for event in backend.drain_events() {
+            data.handle(event);
+        }
+    }
 }
+
+pub struct MouseUpdater {}
+
+impl MouseUpdater {
+    pub fn new() -> Self {
+        MouseUpdater {}
+    }
+}
+
+impl<B, D> mursten::Updater<B, D> for MouseUpdater
+where
+    D: mursten::Data + OnMouse,
+    B: mursten::Backend<D> + backend::MouseEventSource,
+{
+    fn update(&mut self, backend: &mut B, data: &mut D) {
+        for event in backend.drain_events() {
+            data.handle(event);
+        }
+    }
+}
+
+// TODO: Maybe implement a GenericUpdater, like an `EventPump<Source, Event, Handler>`.
+//
+// Generally the problem is alvays the same and the same components appear. There is a _Source_
+// from which we can drain events. Then there is _Handler_ which wants to know about those events.
+// To operate them there is an _Updater_ (TODO: think about a better name for it... there can be an
+// interesting metaphor with a pump) which takes the events from the sources and gives them to the
+// Handler. And there is the _Event_ itself.
+
+
+pub mod backend {
+    pub trait KeyboardEventSource {
+        fn drain_events(&mut self) -> Vec<super::KeyboardEvent>;
+    }
+
+    pub trait MouseEventSource {
+        fn drain_events(&mut self) -> Vec<super::MouseEvent>;
+    }
+}
+
