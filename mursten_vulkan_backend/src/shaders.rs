11aad2391d198957a80d5f2029810c3d155a0857
Nikita Zdanovitch <nzdanovitch@gmail.com>
2018-09-01 01:54:38 -0300

    Fix coordinate system to behave like in opengl.

--- mursten_vulkan_backend/src/shaders.rs
+++ mursten_vulkan_backend/src/shaders.rs
@@ -33,17 +33,13 @@ pub mod vs {
         } c;
 
         void main() {
-            mat4 fix_coordinate_system = mat4(
-                 1,  0,  0,  0,
-                 0, -1,  0,  0,
-                 0,  0, -1,  0,
-                 0,  0,  0,  1
-            );
-            gl_Position = c.projection * c.view * c.world * fix_coordinate_system * position;
+            gl_Position = c.projection * c.view * c.world * position;
+            gl_Position.y = -gl_Position.y;
+            gl_Position.z = (gl_Position.z + gl_Position.w) / 2.0;
 
             outColor = color;
 
-            outFragPos = fix_coordinate_system * position;
+            outFragPos = position;
 
             outNormal = normal;
         }
@@ -83,25 +79,18 @@ pub mod fs {
         }
 
         void main() {
-            mat4 fix_coordinate_system = mat4(
-                -1,  0,  0,  0,
-                 0, -1,  0,  0,
-                 0,  0, -1,  0,
-                 0,  0,  0,  1
-            );
-
             vec4 ambient = c.ambient_strength * c.ambient_color;
             ambient.w = 1.0;
 
             vec4 norm = normalize(inNormal);
-            vec4 diffuse_origin = fix_coordinate_system * c.diffuse_origin;
+            vec4 diffuse_origin = c.diffuse_origin;
             vec4 lightDir = normalize(diffuse_origin - inFragPos);  
 
             float diff = max(dot(norm, lightDir), 0.0);
             vec4 diffuse = diff * c.diffuse_color;
 
 
-            vec4 viewPos = fix_coordinate_system * c.view * vec4(0, 0, 0, 1);
+            vec4 viewPos = c.view * vec4(0, 0, 0, 1);
             vec4 viewDir = normalize(viewPos - inFragPos);
             vec4 reflectDir = reflect(-lightDir, norm); 
 
