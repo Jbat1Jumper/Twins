7921481621b9fcf2eea9986c4c1ef8f7e5d345e1
Nikita Zdanovitch <nzdanovitch@gmail.com>
2018-10-01 02:07:24 -0300

    Really dirty draft on how to structure updates and components. Needs more work on improving data binding of structures.

--- drafts/cursive_renderer.rs
+++ drafts/cursive_renderer.rs
@@ -8,9 +8,11 @@ use mursten::dummy::DummyBackend;
 use mursten_blocks::cursive_renderer::{CursiveRenderer, CursiveView, CursiveContext};
 use mursten_blocks::cursive_renderer::cursive::Cursive;
 use mursten_blocks::cursive_renderer::cursive::views::*;
+use mursten_blocks::cursive_renderer::cursive::menu::*;
 use mursten_blocks::cursive_renderer::cursive::traits::*;
 use mursten_blocks::events::simple::EventHandler;
 use mursten_blocks::events::{EventReceiver, EventEmitter};
+use mursten_blocks::events::transport::Address;
 
 
 pub fn main() {
@@ -25,12 +27,40 @@ pub fn main() {
 }
 
 struct Model {
+    patients: Vec<MedicalRecord>,
+}
+
+struct MedicalRecord {
     name: String,
+    surname: String,
+    age: usize,
+    weight: f32,
 }
 
 impl Model {
     pub fn new() -> Self {
-        Self { name: "Pedro".to_owned() }
+        Self { 
+            patients: vec![
+                MedicalRecord {
+                    name: "Pedro".to_owned(),
+                    surname: "Rodriguez".to_owned(),
+                    age: 25,
+                    weight: 93.0,
+                },
+                MedicalRecord {
+                    name: "Homar".to_owned(),
+                    surname: "Sanchez".to_owned(),
+                    age: 38,
+                    weight: 84.2,
+                },
+                MedicalRecord {
+                    name: "Carlos".to_owned(),
+                    surname: "Gutierrez".to_owned(),
+                    age: 31,
+                    weight: 103.1,
+                }
+            ],
+        }
     }
 }
 
@@ -47,7 +77,13 @@ impl View {
 
 #[derive(Clone)]
 enum Action {
-    RandomizeName,
+    ChangeName {
+        record_position: usize,
+        name: String,
+        surname: String,
+    },
+    CreateNew,
+    DeleteRecord(usize),
     Quit,
 }
 
@@ -55,32 +91,91 @@ impl CursiveView for View {
     type Model = Model;
     type Event = Action;
     fn configure(&mut self, ctx: &mut CursiveContext<Self>) {
-        let address = ctx.address();
-        let randomize_name = move |_: &mut Cursive| {
-            address.send(Action::RandomizeName);
-        };
-
-        let address = ctx.address();
-        let request_quit = move |_: &mut Cursive| {
-            address.send(Action::Quit);
-        };
-
+        let addr = ctx.address();
+        let addr_2 = ctx.address();
         let mut s = ctx.screen();
         s.add_layer(
-            Dialog::around(
-                TextView::new("_this will be replaced by the real text_")
-                    .with_id("model.name")
-            )
-                .title("The name in the model")
-                .button("Randomize name", randomize_name)
-                .button("Quit", request_quit)
+            Dialog::new()
+                .title("Meical Record System 2000")
+                .content(
+                    LinearLayout::vertical()
+                        .with(|list| {
+                        })
+                        .with_id("records")
+                )
         );
+        s.menubar()
+            .add_subtree(
+                "Records",
+                MenuTree::new()
+                    .leaf("New", move |_| { addr.send(Action::CreateNew); })
+                    .leaf("Quit", move |_| { addr_2.send(Action::Quit); })
+            );
+        s.set_autohide_menu(false);
     }
     fn update(&mut self, ctx: &mut CursiveContext<Self>, model: &Self::Model) {
+        let addr = ctx.address();
         let mut s = ctx.screen();
-        s.call_on_id("model.name", |tv: &mut TextView| {
-            tv.set_content(model.name.clone());
+        s.call_on_id("records", move |ll: &mut LinearLayout| {
+            while ll.len() > model.patients.len() { ll.remove_child(0); }
+            while ll.len() < model.patients.len() { ll.add_child(PatientView::create()); }
+            for (i, record) in model.patients.iter().enumerate() {
+                PatientView::bind(record, i, addr.clone(), ll.get_child_mut(i).unwrap().as_any_mut().downcast_mut().unwrap());
+            }
         });
+    }
+}
+
+struct PatientView;
+
+impl PatientView {
+    fn create() -> LinearLayout {
+        LinearLayout::vertical()
+            .child(
+                LinearLayout::horizontal()
+                    .child(TextView::new("Name: "))
+                    .child(TextView::new("_").with_id("name"))
+                    .child(TextView::new("  "))
+                    .child(TextView::new("Surname: "))
+                    .child(TextView::new("_").with_id("surname"))
+            )
+            .child(
+                LinearLayout::horizontal()
+                    .child(TextView::new("Age: "))
+                    .child(TextView::new("_ years").with_id("age"))
+                    .child(TextView::new("  "))
+                    .child(TextView::new("Weight: "))
+                    .child(TextView::new("_ kg").with_id("weight"))
+            )
+            .child(
+                LinearLayout::horizontal()
+                    .child(TextView::new(""))
+                    .child(Button::new("Edit", |_| { }).with_id("edit_button"))
+                    .child(TextView::new(" "))
+                    .child(Button::new("Delete", |_| { }).with_id("delete_button"))
+            )
+    }
+    fn bind(record: &MedicalRecord, record_id: usize, addr: Address<Action>, view: &mut LinearLayout) {
+        view.find_id("name",    |tv: &mut TextView| { tv.set_content(record.name.clone()); });
+        view.find_id("surname", |tv: &mut TextView| { tv.set_content(record.surname.clone()); });
+        view.find_id("age",     |tv: &mut TextView| { tv.set_content(format!("{} years", record.age)); });
+        view.find_id("weight",  |tv: &mut TextView| { tv.set_content(format!("{} kg", record.weight)); });
+
+        view.find_id(
+            "edit_button",
+            |b: &mut Button| {
+                b.set_callback(move |_| { });
+            }
+        );
+
+        view.find_id(
+            "delete_button",
+            |b: &mut Button| {
+                b.set_callback(move |_| { 
+                    addr.send(Action::DeleteRecord(record_id));
+                });
+            }
+        );
     }
 }
 
@@ -100,10 +195,23 @@ impl EventHandler for ActionReducer {
             Action::Quit => {
                 backend.quit();
             },
-            Action::RandomizeName => {
-                use rand::Rng;
-                let names = vec!["Pedro", "Juan", "Jose", "Carlos", "Eucebio", "Ignacio"];
-                model.name = rand::thread_rng().choose(&names).unwrap().to_string();
+            Action::ChangeName { .. } => {
+                //use rand::Rng;
+                //let names = vec!["Pedro", "Juan", "Jose", "Carlos", "Eucebio", "Ignacio"];
+                //model.name = rand::thread_rng().choose(&names).unwrap().to_string();
+            },
+            Action::CreateNew => {
+                model.patients.push(
+                    MedicalRecord {
+                        name: "Eucebio".to_owned(),
+                        surname: "Valente".to_owned(),
+                        age: 72,
+                        weight: 67.8,
+                    }
+                )
+            },
+            Action::DeleteRecord(record_id) => {
+                model.patients.remove(record_id);
             },
         }
     }
